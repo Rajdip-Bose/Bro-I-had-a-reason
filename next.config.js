c24278e1a003135fb5d5bbc3148b1d404e4617c9
Your Name <your.email@example.com>
2025-04-14 20:10:42 +0530

    Update Next.js config for local development

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/Bro-I-had-a-reason',
-  assetPrefix: '/Bro-I-had-a-reason/',
 };
 
 module.exports = nextConfig; 
